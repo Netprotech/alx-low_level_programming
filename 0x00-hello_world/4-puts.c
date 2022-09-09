28c338c390fd203c4d86f22501d33b596a73f061
Emmanuel <emmanuelibemonugu@gmail.com>
2022-09-09 11:02:38 -0700

    is

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
-main - prints the phrase "Programming is like building a multilingual puzzle
-Return: 0 if exited properly, non-zero otherwise
+*main - prints the string in the output function
+*this program print Programming is like building a multilingual puzzle
+* Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)
 {
