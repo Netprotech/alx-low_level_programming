b29796589fda83f4c7f20cd67beb1414fb807937
Emmanuel <emmanuelibemonugu@gmail.com>
2022-09-20 10:36:33 -0700

    corrections

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * main - header file for protoypes
- *
- * void protoypes (void)
- */
 void reset_to_98(ent *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
