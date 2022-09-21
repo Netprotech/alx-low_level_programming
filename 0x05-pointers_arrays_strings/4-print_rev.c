1a134e581f8b1d68485ec97433ef67fa677b04b2
Emmanuel <emmanuelibemonugu@gmail.com>
2022-09-20 18:42:04 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,7 @@ void rev_string(char *s)
 {
 	char tmp;
 	int i, len, len1;
-	
+
 	len = 0;
 	len1 = 0;
 
@@ -18,9 +18,9 @@ void rev_string(char *s)
 	{
 		len++;
 	}
-	
+
 	len1 = len - 1;
-	
+
 	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
